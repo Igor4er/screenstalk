e4b666d45621554f12edacf8cd9700d07458b1de
Declonu4 <declon3233@gmail.com>
2023-06-15 19:06:43 +0300

    Added cycle of sending screenshots

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import telebot;
 from telebot import types;
 import time;
-import get_screenshots_bytes;
-from .screenshot import get_screenshots_bytes
+from screenshot import get_screenshots_bytes
+import asyncio
 bot = telebot.TeleBot("6182954429:AAFhs3NF6Diynl2lFBzNvJGpwtTd9GGshMs")
-admins = [1148309975, 1028390936]
+admins = [1028390936, 622662662, 5518302740]
+chatid = -849718605
 @bot.message_handler(commands=['start'])
 def start(message):
   bot.send_message(message.chat.id, text="Приветствую вас, чего желаете?")
@@ -16,9 +17,11 @@ def func(message):
         if(message.text == "get screenshot"):
             png = get_screenshots_bytes()
             bot.send_photo(message.chat.id, png)
+        elif(message.text == "1"):
+            bot.send_message(message.chat.id, text=f'{chat.id}')
             
     else: bot.send_message(message.chat.id, text='Простите, у вас нет доступа')
-            
+    
 def Work():
    try:
      y = bot.polling(none_stop=True, interval=0.5)
@@ -28,15 +31,20 @@ def Work():
       time.sleep(60)
       Work()
 
-Work()
+
 
 def screengrab():
     try:
-        time.sleep(20)
+        print("screenshot sended")
         png = get_screenshots_bytes()
-        bot.send_photo(admins, png)
+        bot.send_photo(chatid, png)
+        time.sleep(20)
         screengrab()
-    except:
+    except Exception as e:
+        print(e)
         time.sleep(60)
-        screengrab()
+        screengrab() 
+        
 screengrab()
+
+Work()
